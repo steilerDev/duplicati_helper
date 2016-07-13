8f0b05e99d259742666b1c1bb05d51c04004c5fc
Frank Steiler <frank@steilerdev.de>
2016-07-13 17:46:25 +0200

    Working on auto-update

--- git-update.sh
+++ git-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURRENT_DIR=$(pwd)
-GIT_DIR=$(readlink -f $0)
+GIT_DIR=$(dirname $(readlink -f $0))
 
 echo "Not updated!"
 echo "current dir: $CURRENT_DIR"
@@ -11,19 +11,23 @@ cd $GIT_DIR
 
 echo "Checking for duplicati_helper updates..."
 git remote update > /dev/null
+if [ $? -eq 0 ]; then
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @{u})
 
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse @{u})
-
-if [ $LOCAL != $REMOTE ]; then
-    echo "Updating duplicati_helper and restarting command..."
-    git pull
-    cd $CURRENT_DIR
-    exec $@
-    exit
+    if [ $LOCAL != $REMOTE ]; then
+        echo "Updating duplicati_helper and restarting command..."
+        git pull
+        cd $CURRENT_DIR
+        exec $@
+        echo "...done"
+        exit
+    else
+        echo "...no update available"
+    fi
 else
-    echo "...no update available"
-    cd $CURRENT_DIR
+    echo "...unable to get update"
 fi
 
+cd $CURRENT_DIR
 echo "current dir: $(pwd)"
