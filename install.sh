ed441bcfbfc43104c52457fff25d4b3ee680f30e
Frank Steiler <frank@steilerdev.de>
2016-07-13 22:07:41 +0200

    First beta release for installer

--- install.sh
+++ install.sh
@@ -4,21 +4,27 @@
 # This file will install mono, duplicati and the duplicati helpers
 ####
 
-# Todo: Check and install: wget git gdb (for duplicati helper) 
-
 DUPLICATI_PATH="/opt/duplicati/"
-DUPLICATI_HELPER_PATH="/opt/duplicati_helper_temp/"
+DUPLICATI_HELPER_PATH="/opt/"
+
+SHUTDOWN_BIN=$(which shutdown)
 
 main () {
     echo "Welcome to the installer for mono, duplicati and the duplicati helpers!"
 
     check_root 
-    #check_dependencies
+    check_dependencies
   
-    #yes_no "Do you want to install the latest mono version?" 1 "install_mono" 
-    #yes_no "Do you want to install duplicati?" 1 "install_duplicati"
-    #yes_no "Do you want to install duplicati helper scripts?" 1 "install_duplicati_helper"
-    #yes_no "Do you want to configure your duplicati helper scripts?" 1 "config_duplicati_helper"
+    yes_no "Do you want to install the latest mono version?" 1 "install_mono" 
+    yes_no "Do you want to install duplicati?" 1 "install_duplicati"
+    yes_no "Do you want to install duplicati helper scripts?" 1 "install_duplicati_helper"
+    yes_no "Do you want to configure your duplicati helper scripts?" 1 "config_duplicati_helper"
+    
+    echo "All finished, enjoy!"
+    echo
+    echo "Made by steilerDev"
+    echo "For support and information go to https://github.com/steilerdev/duplicati_helper/"
+    echo "Licensed under a GNU General Public License, Version 3"
 }
 
 # This function checks if the current user has sudo rights
@@ -119,11 +125,11 @@ install_duplicati_helper () {
 
     if yes_no "Do you want the shutdown delayed, in case a backup job is running?" 1 ; then
         echo -n "Installing shutdown script..."
-        SHUTDOWN_BIN=$(which shutdown)
         sudo mv $SHUTDOWN_BIN ${SHUTDOWN_BIN}-bin
         sudo ln -s ${DUPLICATI_HELPER_PATH}/shutdown $SHUTDOWN_BIN
         sudo chown --reference=${SHUTDOWN_BIN}-bin ${DUPLICATI_HELPER_PATH}/shutdown
         sudo chmod --reference=${SHUTDOWN_BIN}-bin ${DUPLICATI_HELPER_PATH}/shutdown
+        SHUTDOWN_BIN=${SHUTDOWN_BIN}-bin
         echo "Done"
     fi
     echo 
@@ -172,10 +178,103 @@ install_duplicati_helper () {
 }
 
 config_duplicati_helper () {
-    echo "This functionality is not yet implemented"
+    echo "The following steps will configure your duplicati helper, if you don't provide a value, the default value will be used"
+    echo
+
+    print_config_header "Connection settings"
+    config_duplicati_helper_item "BASE_URL" "amzcd:///" "Specify the URL for your backups. Each backup will be put into a subfolder of this directory with its respective name." "NOTE: The helper was only tested with Amazon Cloud Drive, feel free to report problems with other providers using https://github.com/steilerdev/duplicati_helper/issues."
+    config_duplicati_helper_item "AUTH_ID" "" "Specify the authid for your provider" "The token can be obtained from https://duplicati-oauth-handler.appspot.com"
+
+    print_config_header "Log settings"
+    config_duplicati_helper_item "LOG_PATH" "/var/log/" "Specify the directory, where the log files should be written to"
+    config_duplicati_helper_item "LOG_LEVEL" "Warning" "Specify the log level of duplicati" "Available log levels: \"Error\", \"Warning\", \"Information\", \"Profiling\""
+
+    print_config_header "Server settings"
+    config_duplicati_helper_item "SERVER_PORT" "8200" "Specify the port, the webinterface should be accessible from"
+    config_duplicati_helper_item "SERVER_PAS" "Password1!" "Specify the password for the webinterface"
+
+    print_config_header "Helper settings"
+    config_duplicati_helper_item "HELPER_AUTO_UPDATE" "true" "Do you want to enable auto-updates for the helper programs before executing the 'duplicati' command?" "Choose 'true' or 'false'"
+    config_duplicati_helper_item "DUPLICATI_UPDATE_POLICY" "InstallBefore" "Specify the update strategy of duplicati itself" "Available values are: 'CheckBefore', 'CheckDuring', 'CheckAfter', 'InstallBefore', 'InstallDuring', 'InstallAfter', 'Never'"
+
+    echo -n "Finishing configuration..."
+    # Auto configuration of some values, that might change due to installation is done now.
+    set_config_value "DUPLICATI" "mono ${DUPLICATI_PATH}/Duplicati.CommandLine.exe" "This is the path to the duplicati executable"
+    set_config_value "DUPLICATI_SERVER" "mono ${DUPLICATI_PATH}/Duplicati.Server.exe" "This is the path to the duplicati server executable"
+    set_config_value "BACKUP_CONF" "${DUPLICATI_HELPER_PATH}/backup.conf" "This is the path to the backup configuration file"
+    set_config_value "DUPLICATIRC" "${DUPLICATI_HELPER_PATH}/duplicatirc" "This is the path to the duplicatirc file"
+    set_config_value "BACKUP_STATUS_FILE" "${DUPLICATI_HELPER_PATH}/backup.status" " This status file will hold the information about the status of previous backups. Use duplicatirc to display it at log in."
+    set_config_value "SHUTDOWN_BIN" "$SHUTDOWN_BIN" "The path to the original shutdown binary"
+    
+    # Now making sure that all the advanced default variables will be there
+    set_config_value "LOG_PREFIX" "duplicati" "This is the prefix every log file will get. The log files will be named using the following schema: prefix.backupName.log"
+    set_config_value "COMPRESSION_MODULE" '--compression-module=\"7z\" --7z-compression-level=7' "These are command line arguments for duplicati, sorted by topic"
+    set_config_value "ENCRYPTION_MODULE" '--encryption-module=\"aes\"' "These are command line arguments for duplicati, sorted by topic"
+    set_config_value "BACKUP_MODULE" '--number-of-retries=25 --keep-time=\"3M\" --dblock-size=\"200mb\" --snapshot-policy=\"on\"' "These are command line arguments for duplicati, sorted by topic"
+    set_config_value "PID_DIR" "/run/" "The directory to store the PID files of running duplicati jobs"
+    set_config_value "DUPLICATI_PID_PREFIX" "duplicati" "The prefix every duplicati job PID file will get. The following schema will be used: prefix.backupName.pid"
+    set_config_value "SHUTDOWN_PID_PREFIX" "shutdown" "The name of the shutdown postponing script's PID" "NOTE: This should not be the same, or start the same as DUPLICATI_PID_PREFIX, otherwise the shutdown script will wait on itself"
+
+    echo "Done" 
+    echo
+    echo "Now the configuration is done, configure your backup by editing the file ${DUPLICATI_HELPER_PATH}/backup.conf"
+    echo
+}
+
+# Prints a header, shown during configuration
+# $1 Label of header, max lenghts: 25 chars
+print_config_header () {
+    echo "############################"
+    echo -n "# "
+    echo -n "$1                           " | head -c 25
+    echo " #"
+    echo "############################"
+
+}
+
+# This function will configure a single item in the configuration file. The file needs to be located in $DUPLICATI_HELPER_PATH/duplicati.conf 
+# $1 Item name (key in config file)
+# $2 Default value
+# $3 Statements, displayed to the user 
+# $4 Second statement, displayed to the user (optional)
+config_duplicati_helper_item () {
+
+    echo "$3"
+    if [ ! -z "$4" ] ; then
+        echo "$4"
+    fi
+
+    read -p "[$2]: " -e answer
+    if [ -z "$answer" ] ; then
+        answer=$2
+    fi
+    set_config_value "$1" "$answer" "$3" "$4"
+    echo
+}
+
+# This function sets a config value in the config file $DUPLICATI_HELPER_PATH/duplicati.conf
+# $1 The key
+# $2 The value
+# $3 Comment for the key-value pair, in case entry did not exist yet (optional)
+# $4 Comment for the key-value pair, in case entry did not exist yet (optional)
+set_config_value () {
+    CONFIGFILE="${DUPLICATI_HELPER_PATH}/duplicati.conf.test"
+    
+    if grep -q $1 $CONFIGFILE ; then
+        sudo sed -i '/'"$1"'/c\'"$1"'="'"$2"'"' $CONFIGFILE
+    else
+        if [ ! -z $3 ] ; then
+            echo "# $3" >> $CONFIGFILE
+        fi
+        if [ ! -z $4 ] ; then
+            echo "# $4" >> $CONFIGFILE
+        fi
+        echo "$1=\"$2\"" >> $CONFIGFILE
+    fi
 }
 
 # This function will ask the user to specify a path
+# The function will return an existing path without trailing slashes!
 # $1 The question string
 # $2 The variable name, the path will be stored in
 get_path () {
@@ -185,6 +284,7 @@ get_path () {
         echo "The path does not exist, creating it"
         sudo mkdir -p "$path" 
     fi
+    path=${path%/}
     printf -v $2 "$path"
 }
 
